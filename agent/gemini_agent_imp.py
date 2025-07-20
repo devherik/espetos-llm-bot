9ee31994060c682f50294b3a153e55796c2f449d
Herik Colares Rezende <dev.herik@gmail.com>
2025-07-20 06:33:28 -0300

    feat: implement singleton pattern and initialize method in GeminiAgentImp

--- agent/gemini_agent_imp.py
+++ agent/gemini_agent_imp.py
@@ -1,7 +1,25 @@
 from agent.agent import AgentInterface
+from pydantic import SecretStr
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.memory.v2.db.redis import RedisMemoryDb
+from agno.memory.v2.memory import Memory
+from agno.storage.redis import RedisStorage
+from agno.tools.telegram import TelegramTools
+from typing import Optional
+from utils.logger import log_message
 
 class GeminiAgentImp(AgentInterface):
+    _instance: Optional[AgentInterface] = None
+    _agent = None
+    model: str = "gemini-2.5-flash"
 
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    
     async def initialize(self) -> None:
         """Initialize the agent."""
-        pass
+        
