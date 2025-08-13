993a9e352882581b18f494551657850376681a00
Herik Colares Rezende <dev.herik@gmail.com>
2025-08-13 05:32:12 -0300

    chore: Remove deprecated data ingestion and messenger modules; update dependencies

--- data_ingestor/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Data ingestion module for espetos-llm-bot."""
-
-from .mariadb_data_ingestor import MariaDBDataIngestor
-from .data_ingestor import DataIngestorInterface
-
-__all__ = ['MariaDBDataIngestor', 'DataIngestorInterface']
